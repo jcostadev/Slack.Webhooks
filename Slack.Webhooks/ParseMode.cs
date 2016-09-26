7c466c01e109a51446f09fb8b69d76bbd360c32b
jcostadev <devcostajuliana@gmail.com>
2016-09-26 18:46:53 -0300

    First commit

---
+++ Slack.Webhooks/ParseMode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slack.Webhooks {
+
+    public static class ParseMode {
+        public static string Full { get { return "full"; } }
+        public static string None { get { return "none"; } }
+    }
+
+}
